417ff20b32f4340f01ff6712b21b721420d6e50e
github@lp-gamboa <lp.gamboa10@uniandes.edu.co>
2020-08-04 13:22:38 -0500

    Num questions

---
+++ ProblemRandomizer.py
@@ -0,0 +1,220 @@
+import sys
+import BlackboardQuiz as bbq 
+import RandomMathObjects as rmo 
+from numpy import random as nr 
+import json 
+from sympy import sympify, latex, lambdify, expr
+
+# ------------------------------------------------------------------------
+def gen_variable(k, desc, var_values): 
+    var_type = list(desc.keys())[0]
+    print(var_type) 
+    var_params = desc[var_type] 
+    optional = list(var_params.keys()) 
+    subs_values = [(sympify(kk), vv) for kk,vv in var_values.items()] 
+    
+    if var_type=="ENTERO": 
+        min_v, max_v = var_params["min"], var_params["max"]
+        nuevo_entero = nr.choice(max_v+1-min_v)+min_v
+        var_values[k] = nuevo_entero
+        
+    elif var_type=="VECTOR_COLUMNA": 
+        params = { "size": var_params["num_coords"] } 
+        params["max_val"] = (4 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_column_vector( size=params["size"], 
+            max_val=params["max_val"], max_denom=params["max_denom"] ) 
+            
+    elif var_type=="VECTOR_FILA": 
+        params = { "size": var_params["num_coords"] } 
+        params["max_val"] = (4 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_row_vector( size=params["size"], 
+            max_val=params["max_val"], max_denom=params["max_denom"] ) 
+        
+    elif var_type=="COMBINACION_LINEAL":
+        params = { "vectors": var_params["vectores"] } 
+        params["max_val"] = (4 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_lin_comb( vectors=params["vectors"], 
+            max_val=params["max_val"], max_denom=params["max_denom"] ) 
+        
+    elif var_type=="POLINOMIO_MONICO":
+        params = {"degree": var_params["degree"]}
+        later_sub_needed = False 
+        if "variable" in optional and len(var_params["variable"])>1:
+            later_sub_needed = True 
+        params["lin_factors"] = ( False if not "lin_factors" in optional 
+                else sympify(var_params["lin_factors"]).subs(subs_values) )
+        params["variable"] = ('x' if not "variable" in optional else 
+            (var_params["variable"] if len(var_params["variable"])==1 else 
+            't' ))
+        params["max_val"] = (4 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        poly = rmo.gen_monic_poly( params["degree"],
+            lin_factors=params["lin_factors"], variable=params["variable"],
+            max_val=params["max_val"], max_denom=params["max_denom"] ) 
+        if later_sub_needed: 
+            poly = poly.subs(
+            [ ('t',sympify(var_params["variable"]).subs(subs_values)) ] )
+        var_values[k] = poly 
+        
+    elif var_type=="POLINOMIO":
+        params = {"degree": var_params["degree"]}
+        later_sub_needed = False 
+        if "variable" in optional and len(var_params["variable"])>1:
+            later_sub_needed = True 
+        params["lin_factors"] = ( False if not "lin_factors" in optional 
+                else sympify(var_params["lin_factors"]).subs(subs_values) )
+        params["variable"] = ('x' if not "variable" in optional else 
+            (var_params["variable"] if len(var_params["variable"])==1 else 
+            't' ))
+        params["max_val"] = (4 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        poly = rmo.gen_poly( params["degree"],
+            lin_factors=params["lin_factors"], variable=params["variable"],
+            max_val=params["max_val"], max_denom=params["max_denom"] ) 
+        if later_sub_needed: 
+            poly = poly.subs(
+            [ ('t',sympify(var_params["variable"]).subs(subs_values)) ] )
+        var_values[k] = poly
+        
+    elif var_type=="MATRIZ_RANGO": 
+        params = {
+            "size": ( 
+                sympify(var_params["num_filas"]).subs(subs_values), 
+                sympify(var_params["num_columnas"]).subs(subs_values) ), 
+            "rank": sympify(var_params["rank"]).subs(subs_values) }
+        params["max_val"] = (3 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_matrix_rank(size=params["size"], 
+                        rank=params["rank"], max_val=params["max_val"],
+                        max_denom=params["max_denom"])
+                        
+    elif var_type=="MATRIZ_DIAGONAL": 
+        params = { "size":
+                        sympify(var_params["dimension"]).subs(subs_values), 
+            "det": sympify(var_params["determinante"]).subs(subs_values) }
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_diagonal_matrix(size=params["size"], 
+                        det=params["det"], max_denom=params["max_denom"])
+                        
+    elif var_type=="MATRIZ_TRIANGULAR":
+        params = { "size":
+                        sympify(var_params["dimension"]).subs(subs_values), 
+            "det": sympify(var_params["determinante"]).subs(subs_values) }
+        params["upper"] = (True if not "superior" in optional 
+                    else sympify(var_params["superior"]).subs(subs_values))
+        params["max_val"] = (7 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_triang_matrix(size=params["size"], 
+                        det=params["det"], upper=params["upper"],
+                    max_denom=params["max_denom"], max_val=params["max_val"])
+                        
+    elif var_type=="MATRIZ_DETERMINANTE":
+        params = { "size":
+                        sympify(var_params["dimension"]).subs(subs_values), 
+            "det": sympify(var_params["determinante"]).subs(subs_values) }
+        params["max_val"] = (3 if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        params["max_denom"] = (1 if not "max_denom" in optional 
+                    else sympify(var_params["max_denom"]).subs(subs_values))
+        var_values[k] = rmo.gen_diagonal_matrix(size=params["size"], 
+                        det=params["det"], max_denom=params["max_denom"],
+                        max_val=params["max_val"])
+                        
+    elif var_type=="MATRIZ_SIMETRICA": 
+        params = { "size":
+                        sympify(var_params["dimension"]).subs(subs_values)}
+        params["sym"] = (True if not "simetrica" in optional 
+                    else sympify(var_params["simetrica"]).subs(subs_values))
+        params["max_val"] = (e if not "max_val" in optional 
+                    else sympify(var_params["max_val"]).subs(subs_values))
+        var_values[k] = rmo.gen_sym_matrix(size=params["size"], 
+                        sym=params["sym"], max_val=params["max_val"])
+                        
+    elif var_type=="EVALUAR_FUNCION":
+        punto = var_params["punto"]
+        fun_vars = list(punto.keys())
+        val_fun_vars = [(sympify(a), var_values[punto[a]]) for a in fun_vars]
+        expr_fun = var_values[var_params["function"]] 
+        var_values[k] = expr_fun.subs(val_fun_vars)
+        # var_values[k] = ?
+        
+    elif var_type=="EVALUAR_EXPRESION":
+        var_values[k] = sympify(var_params["expr"]).subs(subs_values)
+        
+    elif var_type=="OPERACION_MATRICIAL":
+        matriz = sympify(var_params["matriz"]).subs(subs_values)
+        if var_params["operacion"]=="CALC_NUL":
+            var_values[k]= len(matriz.nullspace())
+        elif var_params["operacion"]=="CALC_RANK":
+            var_values[k]=matriz.rank()
+        else:
+            print("operacion no implementada :c") 
+    else: 
+        print("caso no implementado... :c") 
+    return var_values 
+# ------------------------------------------------------------------------
+
+
+def eval_answer(ans_type, ans_desc, var_values): 
+    # este método solo debe retornar un número o un string segun sea el caso (como sabemos que retornar? buena pregunta) 
+    if ans_type=="NUM":
+        return sympify(ans_desc).subs(var_values)
+    else:
+        return 0
+
+
+def gen_num_qs(pool, q_object): # acá q_object se maneja como un diccionario 
+    for i in range(q_object["cantidad"]):
+        nombre, cuerpo = q_object["id"]+"_"+str(i+1), q_object["cuerpo"]
+        variables, val_variables = q_object["variables"].items(), {} 
+        for k, v in variables:
+            val_variables = gen_variable(k, v, val_variables) 
+            cuerpo = cuerpo.replace("["+k+"]", latex(val_variables[k]))
+        ans_desc_object = q_object["respuesta"]
+        ans_type = q_object["tipo"]
+        respuesta = eval_answer(ans_type, ans_desc_object, 
+                                val_variables) 
+        pool.addNumQ(nombre, cuerpo, respuesta, erramt=0.001) 
+
+
+#def gen_multchoice_qs(pool, q_object):
+
+
+#def gen_fib_qs(pool, q_object):
+
+
+#def gen_numc_qs(pool, q_object): 
+
+
+qt_switcher = {"NUM": gen_num_qs}#, "NUMC": gen_numc_qs, "MC": gen_multchoice_qs, "FIBS": gen_fib_qs} # "ESS", "TF", "FIL" 
+path_json = sys.argv[1] 
+
+with open(path_json,"r") as read_file:  
+    test_info = json.load(read_file)
+    """ test_info is a json object that contains all data needed for the test. """
+    qs_info = test_info["preguntas"] # <- this is a list of json objects.
+    with bbq.Package(test_info["nombre"]) as package: 
+        for i in range(len(qs_info)): 
+            q_actual = qs_info[i] # <- objeto de 'tipo' pregunta 
+            q_type = q_actual["tipo"] 
+            with package.createPool(pool_name = q_actual["id"], description = q_actual["descripcion"], instructions = q_actual["instrucciones"]) as q_pool: 
+                qt_switcher[q_type](q_pool, q_actual) 
+            
--- RandomMathObjects.py
+++ RandomMathObjects.py
@@ -1,12 +1,13 @@
-""" A module that produces 'nice' matrices. """
+""" A module that produces 'nice' random math objects, such as vectors, matrices, polynomials, etc. """
 # import numpy as np 
 from numpy import random as nr 
 from sympy import *
-from typing import Tuple
-
+from typing import Tuple, List
 MatSize = Tuple[int, int]
+Vectors = List[str] 
 
-def gen_matrix_rank(size: MatSize, rank, max_denom: int = 1, max_val: int = 3): 
+
+def gen_matrix_rank(size: MatSize, rank: int, max_denom: int = 1, max_val: int = 3): 
     """ Generates a random (size[0])x(size[1]) matrix with given rank. """
     A, piv = zeros(size[0], size[1]), nr.choice(rank, size[1])
     for i in range(rank):
@@ -14,12 +15,13 @@ def gen_matrix_rank(size: MatSize, rank, max_denom: int = 1, max_val: int = 3):
         for j in range(piv[i]+1, size[1]):
             A[i,j] = nr.choice([a for a in range(-1*max_val + 1, max_val)])
     for i in range(size[0]):
-        j = nr.choice(size[0], 3, replace=False)
+        j = nr.choice(size[0], min(size[0],3), replace=False)
         for k in j:
             if k!=i:
                 A = A.elementary_row_op(op="n->n+km", row=i, row2=k, k=nr.choice(max_val)*nr.choice([-1,1]))
     return A
     
+
 def gen_diagonal_matrix(size: int, det, max_denom: int =1): 
     """ Generates a random diagonal (size)x(size)-matrix with given determinant. """
     A, a, idx = eye(size), nr.choice(size), nr.choice(size, 2*int(size/3), replace=False)
@@ -34,10 +36,11 @@ def gen_diagonal_matrix(size: int, det, max_denom: int =1):
             A = A.elementary_row_op(op="n->kn", row=i, k=0)
     return A
     
+
 def gen_int_triang_matrix(size: int, det, upper=True, max_val: int =7, max_denom: int = 1): 
     """ Generates a random triangular (size)x(size)-matrix with given determinant by taking linear combinations with integer coefficients of the rows of a diagonal matrix. """
     A, idx = gen_diagonal_matrix(size, det, max_denom), nr.randint(size)
-    #A = A.elementary_row_op(op="n->kn", row=idx, k=det) # https://docs.sympy.org/latest/modules/matrices/matrices.html 
+    #A = A.elementary_row_op(op="n->kn", row=idx, k=det) 
     for i in range(size):
         for j in range(i+1,size):
             A = A.elementary_row_op(op="n->n+km", row=i, row2=j, k=nr.choice(max_val)*nr.choice([-1,1]))
@@ -45,6 +48,7 @@ def gen_int_triang_matrix(size: int, det, upper=True, max_val: int =7, max_denom
     if not upper:
         A = A.transpose()
     return A
+
 
 def gen_sq_matrix(size: int, det, max_val: int =3, max_denom: int =1):
     """ Generates a random (size)x(size)-matrix with given determinant produced by taking linear combinations with integer coefficients of the rows of a triangular matrix. """
@@ -61,9 +65,10 @@ def gen_sq_matrix(size: int, det, max_val: int =3, max_denom: int =1):
                 A = A.elementary_row_op(op="n->n+km", row=perm[i], k=c, row2=k)
     return A 
 
+
 def gen_sym_matrix(size: int, sym=True, max_val:int=3):
     """Generates a random integer valued (size)x(size)-matrix either symmetric or antisymmetric.""" 
-    A = gen_int_matrix(size, 0, int(max_val/2)+1) 
+    A = gen_sq_matrix(size, 0, int(max_val/2)+1) 
     if sym: # True = Symmetric
         A = A + A.transpose()
         x = nr.choice(size, 2*int(size/3), replace=False)
@@ -72,4 +77,31 @@ def gen_sym_matrix(size: int, sym=True, max_val:int=3):
     else: # False = Anti-symmetric
         A = A - A.transpose()
     return A 
+
+
+def gen_column_vector(size: int, max_val: int =4, max_denom: int =1):
+    return gen_matrix_rank(size=(size,1), rank=1, max_val=max_val, max_denom=max_denom) 
+    
+
+def gen_row_vector(size: int, max_val: int =4, max_denom: int =1):
+    return gen_column_vector(size=size, max_val=max_val, max_denom=max_denom).T
+    
+    
+def gen_monic_poly(degree: int, lin_factors: bool =False, variable: str ='x', max_val: int =4, max_denom: int =1):
+    x = symbols(variable) 
+    A = gen_sq_matrix(size=degree, det=nr.choice(3), max_val=max_val, max_denom=max_denom) if lin_factors else gen_diagonal_matrix(size=degree, det=nr.choice(max_val), max_denom=max_denom) 
+    return (A.charpoly(x)).as_expr() 
+
+
+def gen_poly(degree: int, lin_factors: bool =False, variable: str ='x', max_val: int =4, max_denom: int =1): 
+    return ((-1)**nr.choice(2))*(nr.choice(max_val)+1)*gen_monic_poly(degree=degree, lin_factors=lin_factors, variable=variable, max_val=max_val, max_denom=max_denom) 
+
+
+def gen_lin_comb(vectors: Vectors, max_val: int =4, max_denom: int=1): 
+    vecs, scalars = Matrix([sympify(a) for a in vectors]), gen_row_vector(size=len(vectors), max_val=max_val, max_denom=max_denom) 
+    return (scalars*vecs)[0]
+    
+
+
+
 
